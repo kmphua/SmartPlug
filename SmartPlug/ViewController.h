d218e843af144e3cd4e5cd3b82984f0c40c4e1cc
Kevin Phua <kmphua@gmail.com>
2015-12-12 08:37:15 +0800

    Initial Commit

---
+++ SmartPlug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartPlug
+//
+//  Created by Kevin Phua on 12/12/15.
+//  Copyright © 2015 Kevin Phua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartPlug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartPlug
+//
+//  Created by Kevin Phua on 12/12/15.
+//  Copyright © 2015 Kevin Phua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
